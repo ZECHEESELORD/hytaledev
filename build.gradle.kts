1669074547fccf0bd861e9dc28901d621e1381ed
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2026-01-18 07:55:26 -0500

    build: modernize IntelliJ Platform Gradle setup
    Switch settings.gradle.kts to intellijPlatform { defaultRepositories() } and ignore /.intellijPlatform/ artifacts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.21" apply false
     kotlin("plugin.serialization") version "2.0.21" apply false
-    id("org.jetbrains.intellij.platform") version "2.1.0" apply false
+    id("org.jetbrains.intellij.platform") apply false
 }
 
 allprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.intellij.platform.gradle.extensions.IntelliJPlatformRepositoriesExtension
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 
 rootProject.name = "hytaledev"
 
@@ -13,13 +13,13 @@ plugins {
     id("org.jetbrains.intellij.platform.settings") version "2.1.0"
 }
 
-val currentSettings = this
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
-        IntelliJPlatformRepositoriesExtension.Companion.register(currentSettings, this).defaultRepositories()
+        intellijPlatform {
+            defaultRepositories()
+        }
     }
 }
 
