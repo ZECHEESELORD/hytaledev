a08b592e154f5c38e96d82983bb78dc61299ac17
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2026-01-23 23:58:14 -0500

    update jdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
 plugins {
     kotlin("jvm") version "2.0.21" apply false
     kotlin("plugin.serialization") version "2.0.21" apply false
@@ -7,4 +11,22 @@ plugins {
 allprojects {
     group = "sh.harold.hytaledev"
     version = "0.1.0-SNAPSHOT"
+}
+
+val projectJdkFeatureVersion = 21
+
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        extensions.configure<KotlinJvmProjectExtension> {
+            jvmToolchain(projectJdkFeatureVersion)
+        }
+    }
+
+    plugins.withId("java") {
+        extensions.configure<JavaPluginExtension> {
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of(projectJdkFeatureVersion))
+            }
+        }
+    }
 }
